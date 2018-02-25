85306f587d40d1cef57fc9d44ab7f87300578eab
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-02-25 18:00:25 +0530

    added basic JWT token authentication for signup route

--- fullstack-tutorials/redux-node-authentication/server/models/user.js
+++ fullstack-tutorials/redux-node-authentication/server/models/user.js
@@ -1,10 +1,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 //Define our model.
 const userSchema = new Schema({
   email:{type:String,unique:true,lowercase:true},  //primary key, always saved as lower case.
   password:String
+});
+
+//On save hook, encrypt password.
+// Before saing a model, this is run.
+userSchema.pre('save', function(next) {
+  //user is an instance here, referred to as this.
+  const user = this;
+
+//generate a salt , run callback.
+  bcrypt.genSalt(10 , function(err,salt){
+    if(err) {return next(err);}
+
+ //hash/encrypt password using salt.
+ //Salt + plain password = salt+hashed password.
+    bcrypt.hash(user.password,salt,null,function(err,hash) {
+      if(err) {return next(err);}
+      user.password = hash;
+      //execute callback passed to function to go ahead with process.
+      next();
+    });
+  });
 });
 
 // Create the model class.
