bb65e635e9a52e22ca80b8749b93a258cc8c2e12
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-02-26 21:12:25 +0530

    Added signup component

--- fullstack-tutorials/redux-node-authentication/client/src/actions/index.js
+++ fullstack-tutorials/redux-node-authentication/client/src/actions/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import  {browserHistory} from 'react-router';
 import { AUTH_USER,
-        AUTH_ERROR } from './types';
+         UNAUTH_USER,
+         AUTH_ERROR } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -37,4 +38,22 @@ export function authError(error) {
      type:AUTH_ERROR,
      payload:error
    };
+}
+
+
+export function signOutUser() {
+  localStorage.removeItem('token');
+  return {type:UNAUTH_USER};
+}
+
+export function signUpUser({email,password}) {
+  return function(dispatch) {
+    axios.post(`${ROOT_URL}/signup`,{ email,password })
+      .then( response => {
+        dispatch({type:AUTH_USER});
+        localStorage.setItem('token',response.data.token);
+        browserHistory.push('/feature');
+      })
+      .catch( response =>    dispatch(authError(response.response.data.error)) )
+  }
 }
